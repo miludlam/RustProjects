f436e820d295026b56dd2aa0b71a37e19fea9b14
Michael Ludlam <miludlam@gmail.com>
2025-03-23 22:14:46 -0400

    exclude cargo generated directory contents

--- guessing_game/target/debug/deps/rand_chacha-c4511b36541210eb.d
+++
@@ -1,9 +0,0 @@
-/Users/mludlam/Dev/workspace/github/RustProjects/guessing_game/target/debug/deps/librand_chacha-c4511b36541210eb.rmeta: /Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/Users/mludlam/Dev/workspace/github/RustProjects/guessing_game/target/debug/deps/librand_chacha-c4511b36541210eb.rlib: /Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/Users/mludlam/Dev/workspace/github/RustProjects/guessing_game/target/debug/deps/rand_chacha-c4511b36541210eb.d: /Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs:
-/Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs:
-/Users/mludlam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/mludlam/Dev/workspace/github/RustProjects/guessing_game/target/debug/guessing_game: /Users/mludlam/Dev/workspace/github/RustProjects/guessing_game/src/main.rs
