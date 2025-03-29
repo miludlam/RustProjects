d552c0c64b635824f48541d493ec8dd7bc09a0f5
Michael Ludlam <michael@realtechvr.com>
2025-03-28 22:00:38 -0400

    Documenting game and remove print statement with secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,19 +5,22 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
+    // Generate a seeded random integer between 1 and 100 inclusive
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-
+    // Game loop
     loop {
         println!("Please input your guess.");
 
+        // Declare a mutable empty string
         let mut guess = String::new();
 
+        // Read input
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
+        // Ignoring a non-number guess and prompting again instead of crashing
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -25,11 +28,13 @@ fn main() {
 
         println!("You guessed: {}", guess);
 
+        // Evaluate the guess against the secret number
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
+                // Exit on success
                 break;
             },
         }
