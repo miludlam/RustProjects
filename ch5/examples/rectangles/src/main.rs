51a17f8b4834acc04d35c7a414f9fcda691c815e
Michael Ludlam <michael@realtechvr.com>
2025-04-27 08:55:48 -0400

    Refactoring with structs

--- ch5/examples/rectangles/src/main.rs
+++ ch5/examples/rectangles/src/main.rs
@@ -3,15 +3,23 @@
  * and is refactored to use tuples and refactored again to
  * use structs
  */
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
