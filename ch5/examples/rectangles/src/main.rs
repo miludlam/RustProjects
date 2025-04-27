6ebbc59b89f891926e028a5fc7e5c3d8bdf9dd57
Michael Ludlam <michael@realtechvr.com>
2025-04-27 15:09:42 -0400

    Adding a can_hold method to Rectangle

--- ch5/examples/rectangles/src/main.rs
+++ ch5/examples/rectangles/src/main.rs
@@ -9,9 +9,16 @@ struct Rectangle {
     height: u32,
 }
 
+// Methods implemented by the Rectangle struct
 impl Rectangle {
+    // Calculates the area of the rectangle
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+    
+    // Determines if the rectangle can contain the second rectangle
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
@@ -20,9 +27,20 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+    
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
