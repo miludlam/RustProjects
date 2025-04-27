a5b4aafca17814eb4ce03c0a25ac3ab8676ff1f6
Michael Ludlam <michael@realtechvr.com>
2025-04-27 08:51:48 -0400

    Simple rectangle area calculation program

--- ch5/examples/rectangles/src/main.rs
+++ ch5/examples/rectangles/src/main.rs
@@ -1,3 +1,18 @@
+/**
+ * An example program that calculates the area of a rectangle
+ * and is refactored into using tuples and refactored again to
+ * use structs
+ */
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
